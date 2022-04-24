5f4d1ba079143e11166ec9858adf6690d2aa1b5f
LilJiggly <58260324+LilJiggly@users.noreply.github.com>
2022-04-24 18:34:12 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -26,7 +26,8 @@ Rood
 
 <details open>
   <summary>
-
+  Voor het vak Interface en Interactie heb ik gekozen voor de opdracht over de Pokémon Eevee. <br>
+  Ik heb hiervoor gekozen, omdat Pokémon een game is die vroeger veel gespeeld heb en omdat de Eevee evolutie mij goed bekend is.
   <img src=""> </summary>
 
   ### De eerste versie/schets:
@@ -36,9 +37,15 @@ Rood
   ### Je ambitie: 
   Aan deze technieken/punten wil ik werken:
   - Display: Flex
+    Voornamelijk wil ik leren positioneren met display flex en justify content etc.
   - Display: Grid
-  - 
-  - ...
+    Ik ga zou graag willen oefenen met het bouwen van een grid. Het liefst werk ik vaak in een verdeling van 3 vlakken en hopelijk gaat dat lukken met display Grid.
+  - Geavanceerde selectoren
+    :nth-of-type was tot een week voor de opdracht nieuw voor mij. Intussen beheers ik de kennis erover, maar ik ben benieuwd hoe dit nu weer werkt met :hover of ::after
+  - Javascript
+    Het is en blijft altijd lastig, maar het is wel altijd een goed idee om toch weer even te kijken hoe werkt.
+  - @keyframes
+    Door veel ervaring met after effects snap ik het principe van keyframes, maar het zal toch wel weer even uitzoeken zijn hoe in css werkt.
  
 </details>
 
@@ -51,7 +58,8 @@ Rood
   <summary>Mijn bevindingen + wijzigingen (minimaal 5)</summary>
 
   ### Bevinding 1:
-  Omschrijving van wat er nog niet orde was (tekst en afbeeding(en)).
+  Zoals ik eerder al zei bij de ambities zou ik graag in een vlakverdeling van 3 willen werken. 
+  <img src="proces/Layout schetsen.png">
 
   #### oplossing:
   Beschrijving hoe je het hebt hebt opgelost of als het niet gelukt is hoe je het zou oplossen (tekst en afbeeding(en)).
@@ -164,8 +172,18 @@ Rood
 Nb. Wees specifiek ('css-tricks' als bron is bijv. niet specifiek genoeg).
 
 1. Info over de Pokémons https://bulbapedia.bulbagarden.net/wiki/ & https://pokemondb.net/pokedex/
-2. 3D sprites
+2. Types image https://twitter.com/RicoPicoProject/status/1261695478120321024/photo/1
 3. Geluiden https://play.pokemonshowdown.com/audio/cries/
 4. Backgrounds https://www.magicpattern.design/tools/css-backgrounds
+5. Startcode voor het grid https://grid.layoutit.com/
+6. 3D Pokémon sprites https://projectpokemon.org/home/docs/spriteindex_148/3d-models-generation-1-pok%C3%A9mon-r90/
+7. Leaves Patterb https://patternpad.com/editor.html
+8. Patronen voor de achtergronden https://codepen.io/fantaghiro/pen/yLoLjg
+9. Gradients https://cssgradient.io/
+10. Sneeuwvlok https://www.onlinewebfonts.com/icon/540450
+11. Bliksem https://www.svgrepo.com/svg/20303/lightning
+
+
+
 
 </details>
--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,7 @@
 
- // largeEevee.classList.toggle("eevee");
- // largeEeveeBG.classList.toggle("eevee");
     
-
-// var largeEevee = document.querySelector(".pokemonImage");
-
+//variabelen
 var largeEeveeBG = document.querySelector(".pokemonContainer");
-
 var naamEeveelution = document.querySelector("InfoContainer section h1");
 
 // All eevee button at the bottom
@@ -37,43 +32,115 @@ menuEeveesItem1.addEventListener("click", functieNaamGlaceon);
 var menuEeveesItem1 = document.querySelector(".menuEevees ul li:nth-of-type(9)");
 menuEeveesItem1.addEventListener("click", functieNaamSylveon);
 
+//old code to click the pokeball to start
+// var menuEeveesItem1 = document.querySelector("div.pokemonContainer");
+// menuEeveesItem1.addEventListener("click", functieNaamEevee);
+
 //All function that change the images inside pokemonContainer
 function functieNaamEevee() {
     largeEeveeBG.dataset.theevee = 'eevee';
+    document.getElementById("naam").innerHTML = names[0];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[0]
+    document.getElementById("type").innerHTML = "Type: " + types[0]
+    document.getElementById("species").innerHTML = "Species: " + species[0] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[0]
+
+    document.getElementById("info").innerHTML = info[0]
+    document.getElementById("evolution").innerHTML = evolution[0]
+    document.getElementById("notsureyet").innerHTML = notsureyet[0]
     
 };
 
 function functieNaamVaporeon() {
     largeEeveeBG.dataset.theevee = 'vaporeon';
+    document.getElementById("naam").innerHTML = names[1];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[1]
+    document.getElementById("type").innerHTML = "Type: " + types[1]
+    document.getElementById("species").innerHTML = "Species: " + species[1] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[1]
+
+    document.getElementById("info").innerHTML = info[1]
+    document.getElementById("evolution").innerHTML = evolution[1]
+    document.getElementById("notsureyet").innerHTML = notsureyet[1]
 };
 
 function functieNaamJolteon() {
     largeEeveeBG.dataset.theevee = 'jolteon';
+    document.getElementById("naam").innerHTML = names[2];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[2]
+    document.getElementById("type").innerHTML = "Type: " + types[2]
+    document.getElementById("species").innerHTML = "Species: " + species[2] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[2]
+
+    document.getElementById("info").innerHTML = info[2]
+    document.getElementById("evolution").innerHTML = evolution[2]
+    document.getElementById("notsureyet").innerHTML = notsureyet[2]
 };
 
 function functieNaamFlareon() {
     largeEeveeBG.dataset.theevee = 'flareon';
+    cryOut.dataset.cries = 'flareon';
+    document.getElementById("naam").innerHTML = names[3];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[3]
+    document.getElementById("type").innerHTML = "Type: " + types[3]
+    document.getElementById("species").innerHTML = "Species: " + species[3] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[3]
+
+    document.getElementById("info").innerHTML = info[3]
+    document.getElementById("evolution").innerHTML = evolution[3]
+    document.getElementById("notsureyet").innerHTML = notsureyet[3]
 };
 
 function functieNaamEspeon() {
     largeEeveeBG.dataset.theevee = 'espeon';
+    document.getElementById("naam").innerHTML = names[4];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[4]
+    document.getElementById("type").innerHTML = "Type: " + types[4]
+    document.getElementById("species").innerHTML = "Species: " + species[4] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[4]
+
+    document.getElementById("info").innerHTML = info[4]
+    document.getElementById("evolution").innerHTML = evolution[4]
+    document.getElementById("notsureyet").innerHTML = notsureyet[4]
 };
 
 function functieNaamUmbreon() {
     largeEeveeBG.dataset.theevee = 'umbreon';
+    document.getElementById("naam").innerHTML = names[5];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[5]
+    document.getElementById("type").innerHTML = "Type: " + types[5]
+    document.getElementById("species").innerHTML = "Species: " + species[5] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[5]
+
+    document.getElementById("info").innerHTML = info[5]
+    document.getElementById("evolution").innerHTML = evolution[5]
+    document.getElementById("notsureyet").innerHTML = notsureyet[5]
 };
 
 function functieNaamLeafeon() {
     largeEeveeBG.dataset.theevee = 'leafeon';
+    document.getElementById("naam").innerHTML = names[6];
+    document.getElementById("number").innerHTML = "National Number:	#" + number[6]
+    document.getElementById("type").innerHTML = "Type: " + types[6]
+    document.getElementById("species").innerHTML = "Species: " + species[6] + " pokémon"
+    document.getElementById("color").innerHTML = "Pokédexcolor: " + color[6]
+
+    document.getElementById("info").innerHTML = info[6]
+    document.getElementById("evolution").innerHTML = evolution[6]
+    document.getElementById("notsureyet").innerHTML = notsureyet[6]
 };
 
 function functieNaamGlaceon() {
     largeEeveeBG.dataset.theevee = 'glaceon';
     document.getElementById("naam").innerHTML = names[7];
-    document.getElementById("number").innerHTML = "National Number: #" + number[7] 
+    document.getElementById("number").innerHTML = "National Number:	#" + number[7]
     document.getElementById("type").innerHTML = "Type: " + types[7]
     document.getElementById("species").innerHTML = "Species: " + species[7] + " pokémon"
     document.getElementById("color").innerHTML = "Pokédexcolor: " + color[7]
+
+    document.getElementById("info").innerHTML = info[7]
+    document.getElementById("evolution").innerHTML = evolution[7]
+    document.getElementById("notsureyet").innerHTML = notsureyet[7]
 };
 
 function functieNaamSylveon() {
@@ -84,11 +151,14 @@ function functieNaamSylveon() {
     document.getElementById("species").innerHTML = "Species: " + species[8] + " pokémon"
     document.getElementById("color").innerHTML = "Pokédexcolor: " + color[8]
 
-    document.getElementById("about").innerHTML = about[8]
-    document.getElementById("abilities").innerHTML = "It emits a soothing aura from its ribbon-shaped organs. It wraps these appendages around quarrelers to instantly restore calm to the situation."
-    document.getElementById("evolution").innerHTML = "How to evolve: Eevee into Sylveon. Once your Eevee has at least two Affection hearts and knows a Fairy-type move, level it up. You can do this through random battles, fights with other trainers, candies and so on. Once your Eevee levels up, assuming the conditions above have been met, it should immediately evolve into Sylveon."
+    document.getElementById("info").innerHTML = info[8]
+    document.getElementById("evolution").innerHTML = evolution[8]
+    document.getElementById("notsureyet").innerHTML = notsureyet[8]
 };
 
+
+
+//alle arrays met waardes
 const names = [
     "Eevee",
     "Vaporeon",
@@ -149,16 +219,40 @@ const names = [
       "Pink"
   ]
 
-  const about = [
-      "Eevee (Japanese: イーブイ Eievui) is a Normal-type Pokémon introduced in Generation I. It evolves into one of eight different Pokémon through various methods:",
+  const info = [
+      "Eevee (Japanese: イーブイ Eievui) is a Normal-type Pokémon introduced in Generation I.",
       "Vaporeon (Japanese: シャワーズ Showers) is a Water-type Pokémon introduced in Generation I.",
       "Jolteon (Japanese: サンダース Thunders) is an Electric-type Pokémon introduced in Generation I.",
       "Flareon (Japanese: ブースター Booster) is a Fire-type Pokémon introduced in Generation I.",
       "Espeon (Japanese: エーフィ Eifie) is a Psychic-type Pokémon introduced in Generation II.",
       "Umbreon (Japanese: ブラッキー Blacky) is a Dark-type Pokémon introduced in Generation II.",
       "Leafeon (Japanese: リーフィア Leafia) is a Grass-type Pokémon introduced in Generation IV.",
-      "Glaceon (Japanese: グレイシア Glacia) is an Ice-type Pokémon introduced in Generation IV. It evolves from Eevee when it is leveled up near an Ice Rock (from Generations IV to VII, Brilliant Diamond and Shining Pearl), when exposed to an Ice Stone (Sword and Shield), or either through exposure to an Ice Stone or the evolution being forced in proximity to an Ice Rock (Legends: Arceus). It is one of Eevee's final forms, the others being Vaporeon, Jolteon, Flareon, Espeon, Umbreon, Leafeon, and Sylveon." ,
-      "Sylveon (Japanese: ニンフィア Nymphia) is a Fairy-type Pokémon introduced in Generation VI. It evolves from Eevee when leveled up knowing a Fairy-type move and having either at least two levels of Affection (in Generations VI and VII) or high friendship (Sword and Shield and Legends: Arceus). It is one of Eevee's final forms, the others being Vaporeon, Jolteon, Flareon, Espeon, Umbreon, Leafeon, and Glaceon."
+      "Glaceon (Japanese: グレイシア Glacia) is an Ice-type Pokémon introduced in Generation IV.",
+      "Sylveon (Japanese: ニンフィア Nymphia) is a Fairy-type Pokémon introduced in Generation VI."
+  ]
+
+  const evolution = [
+      "It evolves into one of eight different Pokémon through various methods:",
+      "Water stone",
+      "Fire stone",
+      "Thunder stone",
+      "Max-friendship day",
+      "Max-friendship night",
+      "Damp rock",
+      "Icy rock",
+    "How to evolve: Eevee into Sylveon. Once your Eevee has at least two Affection hearts and knows a Fairy-type move, level it up. You can do this through random battles, fights with other trainers, candies and so on. Once your Eevee levels up, assuming the conditions above have been met, it should immediately evolve into Sylveon."
+  ]
+
+  const notsureyet = [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
   ]
 
   const cries = [
@@ -168,8 +262,36 @@ const names = [
   ]
 
 
-var audio = new Audio(cries[1]);
+// test voor geluid
+var cryOut = document.getElementById("cry");
+cryOut.addEventListener("click", cry);
 
-document.getElementById("cry").onclick = function() {
-  audio.play();
+function cry() {
+document.getElementById("cryPlay").innerHTML = cries[8]
+ console.log("yo")
+}
+
+
+
+//switch tabs
+var infoTab = document.getElementById("infoButton");
+infoTab.addEventListener("click", switchTabs);
+
+var textInTab = document.getElementById("article")
+var nextTab = document.querySelector("ol.evolution")
+
+function switchTabs(){
+    textInTab.classList.toggle('hide')
+    nextTab.classList.toggle('hidden')
+}
+
+var infoTab = document.getElementById("evolutionButton");
+infoTab.addEventListener("click", switchTabs);
+
+var textInTab = document.getElementById("article")
+var nextTab = document.querySelector("ol.evolution")
+
+function switchTabs(){
+    textInTab.classList.toggle('hide')
+    nextTab.classList.toggle('hidden')
 }
